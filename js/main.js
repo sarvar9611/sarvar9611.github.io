64cca91a35cec44429a601e381c1de7d16fd9da4
Sarvarbek Azimov <sarvar9611@yandex.com>
2022-05-31 04:32:16 +0500

    New changes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
-const arr = [];
+const contactsNameArr = [];
+const contactsPhoneArr = [];
+let counter = 0;
 
-function splitArr(params) {
-    arr.push(prompt("Input here:", "").split(","));
+function removeContact(num) {
+    let contactList = document.querySelector('#contactsList'),
+        contact = document.querySelectorAll('li');
+
+    contactList.removeChild(contact[num]);
 }
 
-let isTrue = true;
-for (;isTrue ;) {
-    splitArr();
+function saveContact() {
+    let inputName = document.querySelector('#nameInput');
+    contactsNameArr.push(inputName.value);
 
-    isTrue = confirm("Davom etasizmi?");
+    let inputPhone = document.querySelector('#telInput');
+    contactsPhoneArr.push(inputPhone.value);
+
+    createLi();
+    
 }
 
 
-for (let i of arr) {
-        document.write(`${i}<br>`)
-    }
+function createLi() {
+    let contact =  document.createElement('li'),
+        div1 = document.createElement('div'),
+        div2 = document.createElement('div'),
+        div3 = document.createElement('div');
+    div1.textContent = contactsNameArr[counter];
+    div2.textContent = contactsPhoneArr[counter];
+    div3.innerHTML = `<button class="btn btn-secondary" onclick="removeContact(${counter})">Delete</button>`;
+    contact.append(div1);
+    contact.append(div2);
+    contact.append(div3);
+    document.querySelector('#contactsList').append(contact);
+    counter += 1;
+}
