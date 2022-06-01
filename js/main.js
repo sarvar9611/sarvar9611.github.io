d901424ff677e7be5005a0dbfc5d5b2537faea79
Sarvarbek Azimov <sarvar9611@yandex.com>
2022-06-01 13:01:06 +0500

    new changes

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ let counter = 0;
 
 function removeContact(num) {
     let contactList = document.querySelector('#contactsList'),
-        contact = document.querySelectorAll('li');
+        contact = document.querySelector(`#li${num}`);
 
-    contactList.removeChild(contact[num]);
+    contactList.removeChild(contact);
 }
 
 function saveContact() {
@@ -26,8 +26,10 @@ function createLi() {
         div1 = document.createElement('div'),
         div2 = document.createElement('div'),
         div3 = document.createElement('div');
+    contact.setAttribute("id", `li${counter}`);
     div1.textContent = contactsNameArr[counter];
     div2.textContent = contactsPhoneArr[counter];
+    div2.innerHTML = `<a href="tel:+${contactsPhoneArr[counter]}">+${contactsPhoneArr[counter]}</a>`;
     div3.innerHTML = `<button class="btn btn-secondary" onclick="removeContact(${counter})">Delete</button>`;
     contact.append(div1);
     contact.append(div2);
